b356efbea00174cd204e91bf82a7b4741b300336
batalova-kira <batalovakira@gmail.com>
2024-07-28 16:08:14 +0300

    add favorite icon

--- src/сomponents/NoticesCard/NoticesCard.jsx
+++ src/сomponents/NoticesCard/NoticesCard.jsx
@@ -1,33 +1,84 @@
 import Icon from "../Icon/Icon";
 import {
+    BtnNoticeFavorite,
+    BtnNoticeLearnMore,
+    CommentText,
+    MetricAccentNotice,
+    MetricNotice,
+    MetricNoticeText,
     NoticeImg,
     PopularityNotice,
     Star,
     TitleNotice,
+    WrapperBtnsNotice,
     WrapperHeaderNotice,
+    WrapperMetricsNotice,
     WrapperNoticeCard,
     WrapperNoticeImg,
     WrapperReview,
 } from "./NoticesCard.styled";
 
 export const NoticesCard = ({ noticesItem }) => {
-    const { title, imgURL, popularity, _id } = noticesItem;
+    const {
+        title,
+        imgURL,
+        popularity,
+        _id,
+        name,
+        birthday,
+        sex,
+        species,
+        category,
+        comment,
+    } = noticesItem;
+
+    const formBirthday = birthday.replace(/-/g, ".");
 
     return (
         <WrapperNoticeCard key={_id}>
             <WrapperNoticeImg>
                 <NoticeImg src={imgURL} alt={title} />
             </WrapperNoticeImg>
-
-            <WrapperHeaderNotice>
-                <TitleNotice>{title}</TitleNotice>
-                <WrapperReview>
-                    <Star>
-                        <Icon name="star" width={16} height={16} />
-                    </Star>
-                    <PopularityNotice>{popularity}</PopularityNotice>
-                </WrapperReview>
-            </WrapperHeaderNotice>
+            <div>
+                <WrapperHeaderNotice>
+                    <TitleNotice>{title}</TitleNotice>
+                    <WrapperReview>
+                        <Star>
+                            <Icon name="star" width={16} height={16} />
+                        </Star>
+                        <PopularityNotice>{popularity}</PopularityNotice>
+                    </WrapperReview>
+                </WrapperHeaderNotice>
+                <WrapperMetricsNotice>
+                    <MetricNotice>
+                        <MetricNoticeText>Name</MetricNoticeText>
+                        <MetricAccentNotice>{name}</MetricAccentNotice>
+                    </MetricNotice>
+                    <MetricNotice>
+                        <MetricNoticeText>Birthday</MetricNoticeText>
+                        <MetricAccentNotice>{formBirthday}</MetricAccentNotice>
+                    </MetricNotice>
+                    <MetricNotice>
+                        <MetricNoticeText>Sex</MetricNoticeText>
+                        <MetricAccentNotice>{sex}</MetricAccentNotice>
+                    </MetricNotice>
+                    <MetricNotice>
+                        <MetricNoticeText>Species</MetricNoticeText>
+                        <MetricAccentNotice>{species}</MetricAccentNotice>
+                    </MetricNotice>
+                    <MetricNotice>
+                        <MetricNoticeText>Category</MetricNoticeText>
+                        <MetricAccentNotice>{category}</MetricAccentNotice>
+                    </MetricNotice>
+                </WrapperMetricsNotice>
+                <CommentText>{comment}</CommentText>
+            </div>
+            <WrapperBtnsNotice>
+                <BtnNoticeLearnMore>Learn more</BtnNoticeLearnMore>
+                <BtnNoticeFavorite>
+                    <Icon name="favorite-heart" width={18} height={18} />
+                </BtnNoticeFavorite>
+            </WrapperBtnsNotice>
         </WrapperNoticeCard>
     );
 };
--- src/сomponents/NoticesCard/NoticesCard.styled.js
+++ src/сomponents/NoticesCard/NoticesCard.styled.js
@@ -47,6 +47,7 @@ export const WrapperHeaderNotice = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-bottom: 8px;
 `;
 
 export const TitleNotice = styled.p`
@@ -81,4 +82,68 @@ export const PopularityNotice = styled.span`
     font-size: 14px;
     line-height: 1.29;
     color: ${(props) => props.theme.colors.secondaryBlack};
+`;
+
+export const WrapperMetricsNotice = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 14px;
+    margin-bottom: 16px;
+`;
+
+export const MetricNotice = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+`;
+
+export const MetricNoticeText = styled.p`
+    font-size: 10px;
+    line-height: 1.4;
+    letter-spacing: -0.02em;
+    color: ${(props) => props.theme.colors.lightGrey};
+`;
+
+export const MetricAccentNotice = styled.span`
+    font-size: 12px;
+    line-height: 1.17;
+    letter-spacing: -0.02em;
+    color: ${(props) => props.theme.colors.black};
+`;
+
+export const CommentText = styled.p`
+    font-size: 14px;
+    line-height: 1.29;
+    letter-spacing: -0.02em;
+    color: ${(props) => props.theme.colors.secondaryBlack};
+`;
+
+export const WrapperBtnsNotice = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+`;
+
+export const BtnNoticeLearnMore = styled.button`
+    padding: 14px 80px;
+
+    font-size: 14px;
+    line-height: 1.29;
+    letter-spacing: -0.03em;
+
+    color: ${(props) => props.theme.colors.secondBackground};
+    background-color: ${(props) => props.theme.colors.yellow};
+    border: none;
+    border-radius: 30px;
+    cursor: pointer;
+`;
+
+export const BtnNoticeFavorite = styled.button`
+    padding: 14px;
+
+    color: ${(props) => props.theme.colors.yellow};
+    background-color: ${(props) => props.theme.colors.lightYellow};
+    border: none;
+    border-radius: 30px;
+    cursor: pointer;
 `;
